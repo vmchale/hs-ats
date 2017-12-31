460854e6141b6e6138033fa8ea15bf9b9fe7231d
Vanessa McHale <vanessa.mchale@reconfigure.io>
2017-12-30 22:26:05 -0600

    names &c.

--- bench/Bench.hs
+++ bench/Bench.hs
@@ -11,15 +11,15 @@ main =
                       [ bench "factorial" $ nf factorial 12
                       , bench "hsFactorial" $ nf hsFactorial 12
                       ]
-                , bgroup "choose"
+                , bgroup "combinations"
                       [ bench "choose" $ nf (13 `choose`) 4
                       , bench "hsChoose" $ nf (13 `hsChoose`) 4
                       ]
-                , bgroup "isPrime"
+                , bgroup "primality check"
                       [ bench "isPrime" $ nf isPrime 2017
                       , bench "hsIsPrime" $ nf hsIsPrime 2017
                       ]
-                , bgroup "integerExp"
+                , bgroup "integer exponentiation"
                       [ bench "integerExp" $ nf (integerExp 3) 7
                       , bench "^" $ nf ((3 :: Int) ^) (7 :: Int)
                       ]
