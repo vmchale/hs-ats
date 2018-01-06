1047a2f5a5ca676c92700d338d16340e0c2d39ba
Vanessa McHale <vanessa.mchale@reconfigure.io>
2018-01-05 18:16:38 -0600

    small improvements

--- src/Numeric/Combinatorics.hs
+++ src/Numeric/Combinatorics.hs
@@ -7,4 +7,4 @@ foreign import ccall unsafe factorial_ats :: CInt -> CString
 
 -- | This currently returns a byte array representing the integer value.
 factorialRaw :: Int -> CString
-factorialRaw = factorialRaw_ats . fromIntegral
+factorialRaw = factorial_ats . fromIntegral
