be0995e1debd8131768f93adfdea7dfd1927e5bc
Vanessa McHale <vanessa.mchale@iohk.io>
2018-08-07 16:11:20 -0500

    rearrange

--- gmpint/src/Control/Recursion.hs
+++ gmpint/src/Control/Recursion.hs
@@ -10,17 +10,17 @@ module Control.Recursion ( Fix (..)
 
 import           Control.Arrow ((&&&))
 
-coelgot :: Functor f => ((a, f b) -> b) -> (a -> f a) -> a -> b
-coelgot f g = h where h = f . (id &&& fmap h . g)
-
 newtype Fix f = Fix { unFix :: f (Fix f) }
--- | Catamorphism or generic function fold.
-cata :: Functor f => (f a -> a) -> (Fix f -> a)
-cata a = go where go = a . fmap go . unFix
 
 data ListF a x = Cons a x
                | Nil
                deriving (Functor)
+
+coelgot :: Functor f => ((a, f b) -> b) -> (a -> f a) -> a -> b
+coelgot f g = h where h = f . (id &&& fmap h . g)
+
+cata :: Functor f => (f a -> a) -> (Fix f -> a)
+cata a = g where g = a . fmap g . unFix
 
 project :: [a] -> Fix (ListF a)
 project []     = Fix Nil
