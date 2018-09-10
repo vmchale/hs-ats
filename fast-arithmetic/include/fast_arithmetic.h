cc29a7632751d923255ad49642a891ae57e0e62d
Vanessa McHale <vanessa.mchale@iohk.io>
2018-09-09 20:42:25 -0500

    includes

--- fast-arithmetic/include/fast_arithmetic.h
+++ fast-arithmetic/include/fast_arithmetic.h
@@ -1,2 +1,8 @@
 bool is_prime_ats(int);
 int exp_ats(int, int);
+int totient_ats(int);
+int count_divisors_ats(int);
+int little_omega_ats(int);
+int sum_divisors_ats(int);
+bool is_perfect_ats(int);
+bool coprime_ats(int, int);
