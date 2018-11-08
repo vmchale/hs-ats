06be47251502195e4303dd37ec6aa524979cb6ba
Vanessa McHale <vanessa.mchale@iohk.io>
2018-11-07 18:23:35 -0600

    probably not space leaking but I'm not entirely sure

--- fast-arithmetic/src/Numeric/Combinatorics.hs
+++ fast-arithmetic/src/Numeric/Combinatorics.hs
@@ -24,22 +24,24 @@ import           Numeric.GMP.Types
 import           Numeric.GMP.Utils
 import           System.IO.Unsafe       (unsafePerformIO)
 
-foreign import ccall unsafe double_factorial_ats :: CInt -> Ptr MPZ
-foreign import ccall unsafe factorial_ats :: CInt -> Ptr MPZ
-foreign import ccall unsafe choose_ats :: CInt -> CInt -> Ptr MPZ
-foreign import ccall unsafe catalan_ats :: CInt -> Ptr MPZ
-foreign import ccall unsafe derangements_ats :: CInt -> Ptr MPZ
-foreign import ccall unsafe permutations_ats :: CInt -> CInt -> Ptr MPZ
-foreign import ccall unsafe max_regions_ats :: CInt -> Ptr MPZ
-foreign import ccall unsafe stirling2_ats :: CInt -> CInt -> Ptr MPZ
+foreign import ccall unsafe double_factorial_ats :: CInt -> IO (Ptr MPZ)
+foreign import ccall unsafe factorial_ats :: CInt -> IO (Ptr MPZ)
+foreign import ccall unsafe choose_ats :: CInt -> CInt -> IO (Ptr MPZ)
+foreign import ccall unsafe catalan_ats :: CInt -> IO (Ptr MPZ)
+foreign import ccall unsafe derangements_ats :: CInt -> IO (Ptr MPZ)
+foreign import ccall unsafe permutations_ats :: CInt -> CInt -> IO (Ptr MPZ)
+foreign import ccall unsafe max_regions_ats :: CInt -> IO (Ptr MPZ)
+foreign import ccall unsafe stirling2_ats :: CInt -> CInt -> IO (Ptr MPZ)
 
-conjugateMPZ :: (CInt -> Ptr MPZ) -> Int -> Integer
-conjugateMPZ f n = unsafePerformIO (peekInteger mPtr <* mpz_clear mPtr)
-    where mPtr = f (fromIntegral n)
+conjugateMPZ :: (CInt -> IO (Ptr MPZ)) -> Int -> Integer
+conjugateMPZ f n = unsafePerformIO $ do
+    mPtr <- f (fromIntegral n) -- TODO: does mPtr cause two things to be called tho?
+    peekInteger mPtr <* mpz_clear mPtr
 
-conjugateMPZ' :: (CInt -> CInt -> Ptr MPZ) -> Int -> Int -> Integer
-conjugateMPZ' f n k = unsafePerformIO (peekInteger mPtr <* mpz_clear mPtr)
-    where mPtr = f (fromIntegral n) (fromIntegral k)
+conjugateMPZ' :: (CInt -> CInt -> IO (Ptr MPZ)) -> Int -> Int -> Integer
+conjugateMPZ' f n k = unsafePerformIO $ do
+    mPtr <- f (fromIntegral n) (fromIntegral k)
+    peekInteger mPtr <* mpz_clear mPtr
 
 -- | \\( !n \\)
 --
@@ -64,14 +66,14 @@ permutations = conjugateMPZ' permutations_ats
 
 -- | Stirling numbers of the second kind.
 stirling2 :: Int -> Int -> Integer
-stirling2 = unsafePerformIO .* peekInteger .* on stirling2_ats fromIntegral
+stirling2 = conjugateMPZ' stirling2_ats
 
 factorial :: Int -> Integer
 factorial = conjugateMPZ factorial_ats
 
 -- | \( n!! \)
 doubleFactorial :: Int -> Integer
-doubleFactorial = unsafePerformIO . peekInteger . double_factorial_ats . fromIntegral
+doubleFactorial = conjugateMPZ double_factorial_ats
 
 -- | Compute the maximal number of regions obtained by joining \\( n \\) points
 -- about a circle by straight lines. See [here](https://oeis.org/A000127).
