23c4770339d83de3f1abc21879073c2b7c8d2aff
Vanessa McHale <vamchale@gmail.com>
2019-10-12 17:48:55 -0500

    Don't do that

--- fast-arithmetic/src/Numeric/Combinatorics.hs
+++ fast-arithmetic/src/Numeric/Combinatorics.hs
@@ -32,9 +32,6 @@ foreign import ccall unsafe permutations_ats :: CInt -> CInt -> IO (Ptr MPZ)
 foreign import ccall unsafe max_regions_ats :: CInt -> IO (Ptr MPZ)
 foreign import ccall unsafe stirling2_ats :: CInt -> CInt -> IO (Ptr MPZ)
 
--- TODO: use withForeignPtr instead?
--- foreign import ccall "&__gmpz_clear" mpz_clear :: FunPtr (Ptr GMPInt -> IO ())
-
 conjugateMPZ :: (CInt -> IO (Ptr MPZ)) -> Int -> Integer
 conjugateMPZ f n = unsafeDupablePerformIO $ do
     mPtr <- f (fromIntegral n)
