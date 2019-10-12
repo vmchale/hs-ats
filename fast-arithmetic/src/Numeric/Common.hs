37d43f715bf78f9e2925e41633b4f67cd7f86b98
Vanessa McHale <vamchale@gmail.com>
2019-10-12 17:54:38 -0500

    Fix package bounds

--- fast-arithmetic/src/Numeric/Common.hs
+++ fast-arithmetic/src/Numeric/Common.hs
@@ -2,19 +2,13 @@ module Numeric.Common ( conjugate
                       , asTest
                       ) where
 
-import           Data.Word
 import           Foreign.C
 
-asTest :: Integral a => (CInt -> CUChar) -> a -> Bool
+asTest :: Integral a => (CInt -> CBool) -> a -> Bool
 asTest f = convertBool . f . fromIntegral
 
 conjugate :: (Integral a, Integral b) => (CInt -> CInt) -> a -> b
 conjugate f = fromIntegral . f . fromIntegral
 
-convertBool :: CUChar -> Bool
-convertBool = go . fromIntegral
-    where
-        go :: Word8 -> Bool
-        go 0 = False
-        go 1 = True
-        go _ = error "converBool failed"
+convertBool :: CBool -> Bool
+convertBool = toEnum . fromEnum
--- fast-arithmetic/src/Numeric/NumberTheory.hs
+++ fast-arithmetic/src/Numeric/NumberTheory.hs
@@ -21,8 +21,8 @@ foreign import ccall unsafe totient_ats :: CInt -> CInt
 foreign import ccall unsafe count_divisors_ats :: CInt -> CInt
 foreign import ccall unsafe sum_divisors_ats :: CInt -> CInt
 foreign import ccall unsafe little_omega_ats :: CInt -> CInt
-foreign import ccall unsafe is_perfect_ats :: CInt -> CUChar
-foreign import ccall unsafe is_prime_ats :: CInt -> CUChar
+foreign import ccall unsafe is_perfect_ats :: CInt -> CBool
+foreign import ccall unsafe is_prime_ats :: CInt -> CBool
 foreign import ccall unsafe radical_ats :: CInt -> CInt
 
 -- | Radical of an integer
